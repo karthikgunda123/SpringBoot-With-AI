fd0e5dc4b89dcb9df11c7c67ea8909bfb48afb21
Gunda Karthik <gundakarthik644@gmail.com>
2025-04-24 19:58:07 +0530

    Initialized the email reply springboot project

---
+++ Email Reply/email-reply/src/main/java/gunda/karthik/spring/ai/email_reply/EmailReplyApplication.java
@@ -0,0 +1,13 @@
+package gunda.karthik.spring.ai.email_reply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailReplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailReplyApplication.class, args);
+	}
+
+}
---
+++ Email Reply/email-reply/src/test/java/gunda/karthik/spring/ai/email_reply/EmailReplyApplicationTests.java
@@ -0,0 +1,13 @@
+package gunda.karthik.spring.ai.email_reply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailReplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
